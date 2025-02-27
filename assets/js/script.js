f4b451cbdade697fc1f6087980ec80ff75f40969
Francisco Marfull <marfullsen@gmail.com>
2025-02-26 21:07:21 -0300

    ADD:[Footer and Carrousel] add a standard footer and a boilerplate carrousel.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let options = null;
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
