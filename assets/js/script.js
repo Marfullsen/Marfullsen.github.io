798f4b93877edb18986ac835011a604be38dc994
Francisco Marfull <marfullsen@gmail.com>
2025-03-02 23:09:14 -0300

    ADD:[About] Add a modal to display my profile.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let options = null;
     var elems = document.querySelectorAll('.carousel');
     var instancesCarousel = M.Carousel.init(elems, options);
-    var elems = document.querySelectorAll('.tooltipped');
+    elems = document.querySelectorAll('.tooltipped');
     var instancesTooltip = M.Tooltip.init(elems, options);
+    elems = document.querySelectorAll('.modal');
+    var instancesModal = M.Modal.init(elems, options);
   });
